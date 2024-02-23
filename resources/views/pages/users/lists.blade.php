12cf0c62f174f62bb259e184d9a9338930c7082c
MinHeinKoKo <kokominhein96@gmail.com>
2024-02-23 23:14:14 +0630

    finish create

--- resources/views/pages/users/lists.blade.php
+++ resources/views/pages/users/lists.blade.php
@@ -13,6 +13,7 @@
                         <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Role</th>
                         <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Phone</th>
                         <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Address</th>
+                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Control</th>
                         <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">CreatedAt</th>
 
                     </tr>
@@ -27,72 +28,23 @@
                             <td class="whitespace-nowrap text-center px-4 py-2 text-gray-700">{{ $user->role->name }}</td>
                             <td class="whitespace-nowrap text-center px-4 py-2 text-gray-700">{{ $user->phone }}</td>
                             <td class="whitespace-nowrap text-center px-4 py-2 text-gray-700">{{ $user->address }}</td>
+                            <td class="whitespace-nowrap text-center px-4 py-2 text-gray-700">
+                                <a href="{{ route('edit' , $user->id) }}" class="text-green-400">
+                                    Edit
+                                </a>
+                                /
+                                <form method="post" action="{{ route('destroy', $user->id) }}" class="relative">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="text-red-500">Delete</button>
+                                </form>
+                            </td>
                             <td class="whitespace-nowrap text-center px-4 py-2 text-gray-700">{{ \Illuminate\Support\Carbon::parse($user->created_at)->format('Y m d H:i:s') }}</td>
                         </tr>
                     @endforeach
                     </tbody>
                 </table>
             </div>
-
-            @if($users->links())
-                <div class="rounded-b-lg border-t border-gray-200 px-4 py-2">
-                    <ol class="flex justify-end gap-1 text-xs font-medium">
-                        <li>
-                            <a
-                                href="#"
-                                class="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
-                            >
-                                <span class="sr-only">Prev Page</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    class="h-3 w-3"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                        clip-rule="evenodd"
-                                    />
-                                </svg>
-                            </a>
-                        </li>
-
-                        @foreach($users->links() as $link)
-                            <li>
-                                <a
-                                    href="#"
-                                    class="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
-                                >
-                                    1
-                                </a>
-                            </li>
-                        @endforeach
-
-                        <li>
-                            <a
-                                href="#"
-                                class="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
-                            >
-                                <span class="sr-only">Next Page</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    class="h-3 w-3"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                        clip-rule="evenodd"
-                                    />
-                                </svg>
-                            </a>
-                        </li>
-                    </ol>
-                </div>
-            @else
-                Nope
-            @endif
+            {{ $users->onEachSide(1)->links() }}
     </div>
 @endsection
