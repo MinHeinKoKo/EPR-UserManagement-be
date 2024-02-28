5a666f19ef7d79318ce1a363f5c89ed9c322ee3c
MinHeinKoKo <kokominhein96@gmail.com>
2024-02-28 11:36:04 +0630

    start

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Feature;
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
@@ -16,6 +18,24 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+
+        $features = ['user', 'role' , 'product'];
+        $permissions = ['view', 'create', 'update', 'delete'];
+
+        foreach ($features as $f){
+            $feature = Feature::create([
+                'name' => 'user'
+            ]);
+
+            foreach ($permissions as $p){
+                Permission::create([
+                    'name' => $p,
+                    'feature_id' => $feature->id
+                ]);
+            }
+
+        }
 
 
          \App\Models\User::factory()->create([
