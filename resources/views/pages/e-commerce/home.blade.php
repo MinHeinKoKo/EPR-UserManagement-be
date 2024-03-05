e823a9c62395cd491f160adbd5d03e802f22ed05
MinHeinKoKo <kokominhein96@gmail.com>
2024-03-05 21:28:20 +0630

    good to go home page

--- resources/views/pages/e-commerce/home.blade.php
+++ resources/views/pages/e-commerce/home.blade.php
@@ -1,13 +1,24 @@
 @extends('layout.app')
 @section('title', "Mobile E-commerce")
 @section('content')
-    <div class="w-full h-auto flex">
-        <div class="w-64">
+    <div class="w-full h-auto flex gap-x-4">
+        <div class="w-72 my-5 flex flex-col gap-y-5">
+            <x-search-input />
 {{--            displaying category lists--}}
-            <x-ecommerce.category />
-        </div>
-        <div class="w-full h-auto flex flex-col">
 
+            @foreach($categories as $category)
+                <x-ecommerce.category :category="$category" />
+            @endforeach
+
+
+
+        </div>
+        <div class="w-full h-auto flex gap-x-2 my-5 flex-row flex-wrap">
+            @foreach($products as $product)
+                <div class="w-[250px] mb-3">
+                    <x-ecommerce.product :product="$product" />
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
