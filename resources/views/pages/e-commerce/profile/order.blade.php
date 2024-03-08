e35ecd251051095d3d1e8a1f298f84e3de87c165
MinHeinKoKo <kokominhein96@gmail.com>
2024-03-08 20:28:05 +0630

    finsih ui

--- resources/views/pages/e-commerce/profile/order.blade.php
+++ resources/views/pages/e-commerce/profile/order.blade.php
@@ -1,93 +1,125 @@
-@extends('layout.app')
+@extends('layout.profile')
 @section('title', "Mobile E-commerce |Profile")
 @section('content')
-<main class="max-w-7xl h-auto w-full mx-auto">
-  <section class="relative block h-[500px]">
-    <div class="absolute top-0 w-full h-full bg-center bg-cover" style="
-            background-image: url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80');
-          ">
-      <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
+
+<div class="rounded-lg border border-gray-200">
+    <div class="overflow-x-auto rounded-t-lg">
+      <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+        <thead class="ltr:text-left rtl:text-right">
+          <tr>
+            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">#</th>
+            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Name</th>
+            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Color</th>
+            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Quantity</th>
+            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Price</th>
+            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Time</th>
+          </tr>
+        </thead>
+  
+        <tbody class="divide-y divide-gray-200">
+          <tr>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">1</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center font-medium text-gray-900">Samsung</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">Silver</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">1</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">$1250</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">24/05/1995</td>
+          </tr>
+          <tr>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">2</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center font-medium text-gray-900">Samsung</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">Silver</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">1</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">$1250</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">24/05/1995</td>
+          </tr>
+          <tr>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">3</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center font-medium text-gray-900">Samsung</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">Silver</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">1</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">$1250</td>
+            <td class="whitespace-nowrap px-4 py-2 text-center text-gray-700">24/05/1995</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <div class="top-auto bottom-0 left-0 right-0 w-full absolute overflow-hidden h-70-px" style="transform: translateZ(0px)">
-      <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-        <polygon class="text-blue-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
-      </svg>
-    </div>
-  </section>
-  <section class="relative py-16 bg-blueGray-200">
-    <div class="container mx-auto px-4">
-      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-        <div class="px-6">
-          <div class="flex flex-wrap justify-center">
-            <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-              <div class="relative">
-                <img alt="..." src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg" class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-10 max-w-[250px]">
-              </div>
-            </div>
-            <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-              <div class="py-6 px-3 mt-32 sm:mt-0">
-                <button class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                  Connect
-                </button>
-              </div>
-            </div>
-            <div class="w-full lg:w-4/12 px-4 lg:order-1">
-              <div class="flex justify-center py-4 lg:pt-4 pt-8">
-                <div class="mr-4 p-3 text-center">
-                  <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span><span class="text-sm text-blueGray-400">Friends</span>
-                </div>
-                <div class="mr-4 p-3 text-center">
-                  <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">10</span><span class="text-sm text-blueGray-400">Photos</span>
-                </div>
-                <div class="lg:mr-4 p-3 text-center">
-                  <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span><span class="text-sm text-blueGray-400">Comments</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="text-center mt-12">
-            <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-              Jenna Stones
-            </h3>
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-              <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-              Los Angeles, California
-            </div>
-            <div class="mb-2 text-blueGray-600 mt-10">
-              <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Solution Manager - Creative Tim Officer
-            </div>
-            <div class="mb-2 text-blueGray-600">
-              <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i>University of Computer Science
-            </div>
-          </div>
-          <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
-            <div class="flex flex-wrap justify-center">
-              <div class="w-full lg:w-9/12 px-4">
-                <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-                  An artist of considerable range, Jenna the name taken by
-                  Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                  performs and records all of his own music, giving it a
-                  warm, intimate feel with a solid groove structure. An
-                  artist of considerable range.
-                </p>
-                <a href="#pablo" class="font-normal text-pink-500">Show more</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <footer class="relative bg-blueGray-200 pt-8 pb-6 mt-8">
-  <div class="container mx-auto px-4">
-    <div class="flex flex-wrap items-center md:justify-between justify-center">
-      <div class="w-full md:w-6/12 px-4 mx-auto text-center">
-        <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
-        </div>
-      </div>
+  
+    <div class="rounded-b-lg border-t border-gray-200 px-4 py-2">
+      <ol class="flex justify-end gap-1 text-xs font-medium">
+        <li>
+          <a
+            href="#"
+            class="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+          >
+            <span class="sr-only">Prev Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </li>
+  
+        <li>
+          <a
+            href="#"
+            class="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+          >
+            1
+          </a>
+        </li>
+  
+        <li class="block size-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white">
+          2
+        </li>
+  
+        <li>
+          <a
+            href="#"
+            class="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+          >
+            3
+          </a>
+        </li>
+  
+        <li>
+          <a
+            href="#"
+            class="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+          >
+            4
+          </a>
+        </li>
+  
+        <li>
+          <a
+            href="#"
+            class="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+          >
+            <span class="sr-only">Next Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3 w-3"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+        </li>
+      </ol>
     </div>
   </div>
-</footer>
-  </section>
-</main>
 @endsection
